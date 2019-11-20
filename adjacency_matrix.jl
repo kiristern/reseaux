a08f87781d7dd06e98a6071d8b78f77eea88626d
Kiri <kiri.a.stern@gmail.com>
2019-11-20 17:20:23 -0500

    remove unnecessary code

--- adjacency_matrix.jl
+++ adjacency_matrix.jl
@@ -2,56 +2,40 @@ include("required.jl")
 
 transect
 
+#Create Adjacency Matrix
 #create vertices
 G = Graph()
 add_vertices!(G, 5289)
-
 #assign edges
-edges_t =
 
 id_dict = Dict(id => indexin(id, unique_ids) for id in edgelist_T.PredTSN)
 
-for i in 1:length(edgelist_T)
-    for j in 1:nrow(edgelist_T)
-        add_edge!(G, edgelist_T[i][j])
+for i in 1:length(transect)
+    for j in 1:nrow(transect)
+        add_edge!(G, transect[i][j])
     end
 end
+
+
+#plot
+gplot(G, nodelabel=1:nv(G), edgelabel=1:ne(G))
+
 
 
 # create adjency matrix from edge list
 
 
 # graph Adjacency matrix
-Graph(edgelist_T)
+Graph(transect)
 
 
-#test
-G = Graph()
-add_vertices!(G, 5289)
-add_edge!(G, 93294, 10824)
-add_edge!(G, 93294, 11334)
-add_edge!(G, 70395, 11334)
-add_edge!(G, 92283, 11334)
-add_edge!(G, 69731, 11334)
-add_edge!(G, 93294, 12322)
-
-gplot(G, nodelabel=1:nv(G), edgelabel=1:ne(G))
-
-G = Graph()
-add_vertices!(G, 6)
-add_edge!(G, 1, 2)
-add_edge!(G, 1, 3)
-add_edge!(G, 4, 3)
-add_edge!(G, 4, 5)
-add_edge!(G, 5, 2)
-
-gplot(G, nodelabel=1:nv(G), edgelabel=1:ne(G))
 
 ##Test add_edge function
 df_simple = DataFrame(C = [1,1,2,3], D = [3,4,4,5])
 #add new column with edge assignment ?? DOESNT WORK :(
 # df_simple[:E] = add_edge!(G, df_simple.C, df_simple.D)
 #try to assign edge between pairs of vertices
+
 for m in 1:length(df_simple)
     for n in 1:nrow(df_simple)
         add_edge!(G, df_simple[m][n])
@@ -62,29 +46,3 @@ end
 df_copy = copy(df_test)
 unique(df_test.A) .= 1:4
 df_test.A .= 1:nrow(df_copy)
-
-
-# Create df
-df_test = DataFrame(A = [93294, 93294, 70395, 92283, 69731], B = [10824, 11334, 11334, 11334, 12322])
-# Sort by ID
-sort!(df_test, :A)
-# Get unique IDs
-unique_ids = unique(df_test.A)
-# Create Dict where id => newid
-id_dict = Dict(id => indexin(id, unique_ids) for id in df_test.A)
-# Add new ids in dataframe
-df_test.newcol = [id_dict[id] for id in df_test.A]
-df_test
-
-## Repeat for B
-# Sort by ID
-sort!(df_test, :B)
-# Get unique IDs
-unique_ids_B = unique(df_test.B)
-# Create newids
-newids_B = collect(length(unique_ids)+1:length(unique_ids)+length(unique_ids_B))
-# Create Dict
-id_dict_B = Dict(unique_ids_B[i] => newids_B[i] for i in 1:length(unique_ids_B))
-# Add new ids in dataframe
-df_test.newcol2 = [id_dict_B[id] for id in df_test.B]
-df_test
