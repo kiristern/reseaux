d048a8203371b3548b50797b917bed0612056f2e
Kiri <kiri.a.stern@gmail.com>
2019-11-28 21:10:32 -0500

    add plot titles and save

--- null.jl
+++ null.jl
@@ -6,7 +6,7 @@ using Statistics
 # U = UnipartiteNetwork(rand(Bool, (10,10)))
 #U = nz_stream_foodweb()[5]
 
-null_sample = rand(null1(B_A), 10_000)
+null_sample = rand(null2(B_A), 10_000)
  simplify!.(null_sample)
 
 is_a_valid_sample(x, N) = (richness(x) == richness(N)) && (links(x) == links(N))
@@ -16,9 +16,12 @@ is_a_valid_sample_of_U(x) = is_a_valid_sample(x, B_A)
 expected_spectral_radius = ρ.(null_sample)
 # expected_mod = Q(brim(lp(null_sample...))...)
 
-density!(
+null_graph = density!(
     (ρ(B_A) .- expected_spectral_radius)./std(expected_spectral_radius),
     frame=:zerolines)
+plot!(title = "Modèle Null Type 2 d'Emboîtement de l'Archipel", xlabel = "x", ylabel = "f(x)", leg = false)
+
+png("null.png")
 
 # density!(
 #     ( Q_A .- expected_mod)./std(expected_mod),
